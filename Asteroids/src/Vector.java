0bdf6a8f30d4f5ecf612d2aa8950b7aedc3fac82
andrew.shine <andrew.shine@MU-C1-10.mullen.pvt.k12.co.us>
2018-12-04 14:58:02 -0700

    Finished vector class

--- Asteroids/src/Vector.java
+++ Asteroids/src/Vector.java
@@ -1,38 +1,59 @@
 
 public class Vector
 	{
-		int x;
-		int y;
-		int r;
-		double o;
+		double xr;
+		double yo;
 		boolean isXY;
 		
-		public Vector(int x, int y)
+		public Vector(double xr, double yo, boolean isXY)
 		{
-			this.x = x;
-			this.y = y;
-			isXY = true;
-		}
-		public Vector(int r, double o)
-		{
-			this.r = r;
-			this.o = o;
-			isXY = false;
+			this.xr = xr;
+			this.yo = yo;
+			this.isXY = isXY;
 		}
 		
-		public Vector switchTypes()
+		
+		public double getXr()
+			{
+				return xr;
+			}
+		public void setXr(double xr)
+			{
+				this.xr = xr;
+			}
+		public double getYo()
+			{
+				return yo;
+			}
+		public void setYo(double yo)
+			{
+				this.yo = yo;
+			}
+		public boolean isXY()
+			{
+				return isXY;
+			}
+		public void setXY(boolean isXY)
+			{
+				this.isXY = isXY;
+			}
+		public void switchTypes()
 		{
 			if(isXY)
 				{
-					double theta = Math.atan((double)(y/x));
-					int magnitude = (int) Math.sqrt((Math.pow((double) x, 2)) + (Math.pow((double) x, 2)));
-					return new Vector(magnitude, theta);
+					double theta = Math.atan((double)(yo/xr));
+					double magnitude = Math.sqrt((Math.pow((double) xr, 2)) + (Math.pow((double) yo, 2)));
+					setXr(magnitude);
+					setYo(theta);
+					setXY(false);
 				}
 			else
 				{
-					int newX = 0;
-					int newY = 0;
-					return new Vector(newX, newY);
+					double newX = (Math.sin(yo) * xr);
+					double newY = (Math.cos(yo) * xr);
+					setXr(newX);
+					setYo(newY);
+					setXY(true);
 				}
 		}
 	}
