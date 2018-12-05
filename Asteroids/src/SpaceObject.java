1a2e2e18b66da30451b85bba7a475cc11b884014
joshua.obrecht <joshua.obrecht@MU-C1-11.mullen.pvt.k12.co.us>
2018-12-05 14:38:15 -0700

    finished ship

--- Asteroids/src/SpaceObject.java
+++ Asteroids/src/SpaceObject.java
@@ -1,6 +1,10 @@
 
 public abstract class SpaceObject 
 	{
+		protected int xCord[] = {650, 600, 600};
+		protected int yCord[] = {450, 435, 465};
+		protected int numPoints=3;
+		
 		Vector pos;
 		Vector vel;
 		Vector acc;
@@ -15,4 +19,34 @@ public abstract class SpaceObject
 		{
 			return null;
 		}
+		
+		public int[] getxCord()
+			{
+				return xCord;
+			}
+
+		public void setxCord(int[] xCord)
+			{
+				this.xCord = xCord;
+			}
+
+		public int[] getyCord()
+			{
+				return yCord;
+			}
+
+		public void setyCord(int[] yCord)
+			{
+				this.yCord = yCord;
+			}
+
+		public int getNumPoints()
+			{
+				return numPoints;
+			}
+
+		public void setNumPoints(int numPoints)
+			{
+				this.numPoints = numPoints;
+			}
 	}
